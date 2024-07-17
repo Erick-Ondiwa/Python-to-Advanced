261ebaf0a379278b5ab5951596787c8991ce0f5e
erick-ondiwa <erickochieng3860@gmail.com>
2024-07-17 03:54:42 +0300

    Created the first python project of Rock-Paper-Scissors game

--- practice_projects/project1.py
+++ practice_projects/project1.py
@@ -1,48 +1,3 @@
-# #  program to compute the area volume of a cylinder
-#
-# # A function to compute the cross-section area of a circle
-# def compute_circle_area(radius):
-#     pi = 3.142
-#
-#     return pi * radius * radius
-# def compute_volume():
-#     height = 40
-#     area = compute_circle_area(14)
-#     return area * height
-#
-#
-# volume = compute_volume()
-# print(volume)
-#
-#
-# def compute_sum(*numbers):
-#     total = 0
-#     for nums in numbers:
-#         total += nums
-#     print(total)
-#
-#
-# compute_sum(10, 20, 30, 40, 50)
-#
-#
-# def sayHi(**fullname):
-#     print("Dear", end=" ")
-#     for key, value in fullname.items():
-#         print(value, end=" ")
-#     print(",")
-#
-#
-# sayHi(tittle="Eng.", first_name="Erick", middle_name="Ochieng", last_name="Ondiwa")
-#
-# import os
-#
-# file_path = "C:\\Users\\Erick Ondiwa'z\\Desktop\\trial.txt"
-#
-# # Checking if the file exists
-# if os.path.exists(file_path):
-#     print("The specified file exists")
-# else:
-#     print("Could not trace the specified file path")
 import random
 
 
@@ -103,14 +58,3 @@ def display_result():
 
 
 display_result()
-
-
-def compute_attempts():
-    attempts = 0
-    display_result()
-    while display_result():
-        attempts += 1
-    print(attempts)
-
-
-
