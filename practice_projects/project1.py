6d3113af212b11bdb3dab933f121e779fd804dfb
erick-ondiwa <erickochieng3860@gmail.com>
2024-08-29 09:11:05 +0300

    Modified

--- practice_projects/project1.py
+++ practice_projects/project1.py
@@ -8,23 +8,23 @@ def play_game():
     while user_choice not in choices:
         user_choice = input("What's your choice,Rock, Paper or Scissors?: ").lower()
 
-    result = " "
-    if user_choice == computer:
-        result = "You Tie!"
-    elif user_choice == "rock" and computer == "paper":  # 1
-        result = "You Lose!"
-    elif user_choice == "rock" and computer == "scissors":  # 2
-        result = "You Win!"
-    elif user_choice == "paper" and computer == "rock":  # 3
-        result = "You Win!"
-    elif user_choice == "paper" and computer == "scissors":  # 4
-        result = "You Lose!"
-    elif user_choice == "scissors" and computer == "Rock":  # 5
-        result = "You Win!"
-    elif user_choice == "scissors" and computer == "paper":
-        result = "You Lose!"
-    print("You: " + user_choice + ", " + "Computer:" + computer)
-    return result
+        result = " "
+        if user_choice == computer:
+            result = "You Tie!"
+        elif user_choice == "rock" and computer == "paper":  # 1
+            result = "You Lose!"
+        elif user_choice == "rock" and computer == "scissors":  # 2
+            result = "You Win!"
+        elif user_choice == "paper" and computer == "rock":  # 3
+            result = "You Win!"
+        elif user_choice == "paper" and computer == "scissors":  # 4
+            result = "You Lose!"
+        elif user_choice == "scissors" and computer == "Rock":  # 5
+            result = "You Win!"
+        elif user_choice == "scissors" and computer == "paper":
+            result = "You Lose!"
+        print("You: " + user_choice + ", " + "Computer:" + computer)
+        return result
 
 
 class Scores:
@@ -34,19 +34,12 @@ class Scores:
     attempts = 0
 
 
-def play_again():
-    user_response = input("Play again? yes/no: ").upper()
-    if user_response == "YES":
-        display_result()
-    else:
-        print("Goodbyeee")
-
-
 def display_result():
     while Scores.attempts <= 2:
         Scores.attempts += 1
         result = play_game()
         print(result)
+
         if result == "You Win!":
             Scores.wins += 1
         elif result == "You Lose!":
@@ -57,17 +50,27 @@ def display_result():
             print("This is an invalid result")
         print("Wins: " + str(Scores.wins) + " Losses " + str(Scores.losses) + " Ties " + str(
             Scores.ties))
-        
-    print("You've reached a maximum of "+str(Scores.attempts)+" attempts")
-
+    print("You've reached a maximum of " + str(Scores.attempts) + " attempts")
     play_again()
+
     return Scores
 
 
-while display_result():
-    play_again()
-print("Goodbyeeee!")
+display_result()
 
 
+def play_again():
+    user_response = input("Play again? yes/no: ").upper()
+    while True:
+        if user_response == "NO":
+            print("Goodbyeee")
+            break
+        else:
+            display_result()
+# while play_again():
+#     display_result()
 
 
+# while display_result():
+#     play_again()
+# print("Goodbyeeee!")
