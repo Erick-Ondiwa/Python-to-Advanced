0311b3f7e0c1ad046dcb816861ed2bae2d03ca9d
erick-ondiwa <erickochieng3860@gmail.com>
2024-09-05 16:54:17 +0300

    Modified

--- diving_into_Python/python_OOP/classes_vs_objects.py
+++ diving_into_Python/python_OOP/classes_vs_objects.py
@@ -1,9 +1,10 @@
 # An object represent any real world thing
 # Objects are often characterised by their properties(attributes) and methods(functions)
-# The object's attributes describes the features that the object can have while methods are the functionalities
+# The object's attributes describes the features that the object can have while methods are the
+# functionalities
 # associated with the object
 
-# Objects are also associated with classes
+# To create many objects, we need a class are also associated with classes
 # The necessity of a class comes in especially where we have many objects.
 # Thus, an object is simply an instance of a class
 
@@ -25,4 +26,17 @@ my_age = person1.age
 gender1 = person1.name
 interest = person1.person_interest("dog")
 print(interest)
+
+
+class Car:
+    def __init__(self, model, year, color, for_sale):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
+
+
+car1 = Car("BMW", 2024, "Black", True)
+print(car1.model)
+
 
