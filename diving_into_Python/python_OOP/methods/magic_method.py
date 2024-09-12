02042be023144eb4f5072644fc1d834322915a25
erick-ondiwa <erickochieng3860@gmail.com>
2024-09-12 06:26:50 +0300

    Modified

--- diving_into_Python/python_OOP/methods/magic_method.py
+++ diving_into_Python/python_OOP/methods/magic_method.py
@@ -2,3 +2,17 @@
 # They are defined by built-in classes in Python and commonly used for operator overloading.
 #
 # They are also called Dunder methods, Dunder here means “Double Under (Underscores)”.
+
+class Book:
+    def __init__(self, title, author, no_of_pages):
+        self.title = title
+        self.author = author
+        self.no_of_pages = no_of_pages
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+book1 = Book(title="A Doll's House", author="Henrik Ibsen", no_of_pages=176)
+print(book1)
+
