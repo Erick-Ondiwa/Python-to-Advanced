c50cf3c44ca884de6b6d1fb659d94a66b9fcfc4b
erick-ondiwa <erickochieng3860@gmail.com>
2024-09-14 21:27:42 +0300

    Modified

--- diving_into_Python/python_OOP/methods/magic_method.py
+++ diving_into_Python/python_OOP/methods/magic_method.py
@@ -1,6 +1,6 @@
 # Python Magic methods are the methods starting and ending with double underscores ‘__’.
 # They are defined by built-in classes in Python and commonly used for operator overloading.
-#
+# * These methods allow you to emulate the behavior of built-in types
 # They are also called Dunder methods, Dunder here means “Double Under (Underscores)”.
 
 class Book:
@@ -16,6 +16,23 @@ class Book:
     def __eq__(self, other):
         return self.title == other.title and self.author == other.author
 
+# This method is used to check whether certain object attributes are less than the others
+    def __lt__(self, other):
+        return self.no_of_pages < other.no_of_pages
+
+    def __gt__(self, other):
+        return self.no_of_pages < other.no_of_pages
+
+    def __add__(self, other):
+        return self.no_of_pages + other.no_of_pages
+
+    def __contains__(self, item):
+        return item in self.title or item in self.author
+
+    # This method is used to retrieve an attribute by key
+    def __getitem__(self, item):
+        return
+
 
 book1 = Book(title="A Doll's House", author="Henrik Ibsen", no_of_pages=176)
 book2 = Book(title="Blossoms of the Savanna", author="H.R Olekulet", no_of_pages=390)
@@ -24,8 +41,13 @@ book4 = Book(title="The Pearl", author="Martin Luther", no_of_pages=210)
 book5 = Book(title="The River Between", author="S.K Erick", no_of_pages=256)
 
 books = [book1, book2, book3, book4, book5]
-print(book1 == book2)
+idx = 0
 for book in books:
+    # book[2] += 1
     print(book)
+
+print(book1 == book2)
+print(book1 < book2)
+print(book1 + book2)
 
 
