1fc5f85267c4340fc57859ccfda9d7f6213d5f4e
erick-ondiwa <erickochieng3860@gmail.com>
2024-09-17 23:59:43 +0300

    Modified

--- Python_Basics/hello.py
+++ Python_Basics/hello.py
@@ -4,3 +4,21 @@ print("Hello World")
 
 name = "Erick Ondiwaz"
 print("Hello "+name)
+
+names = ["Erick Ondiwa", "James Wasonga", "Felix Awere", "Shadrack Mutinda", "Mark Emmanuel"]
+
+year_of_births = [2000, 2001, 2002, 2003, 2004]
+
+# for year_of_birth in year_of_births:
+#     print(year_of_birth)
+
+name_idx = 0
+
+for name in names:
+    years = year_of_births[name_idx]
+    print(f"{name}:{years}")
+
+    name_idx += 1
+
+
+
