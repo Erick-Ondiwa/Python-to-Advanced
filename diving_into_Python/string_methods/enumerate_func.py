ba4523bee0562868009757283c361cf523f3c27b
erick-ondiwa <erickochieng3860@gmail.com>
2024-09-28 03:24:08 +0300

    Modified

--- diving_into_Python/string_methods/enumerate_func.py
+++ diving_into_Python/string_methods/enumerate_func.py
@@ -2,6 +2,8 @@
 # iterable (such as a list, tuple, or string) while also keeping track of the current index (
 # position) in the iterable. It essentially adds a counter to an iterable and returns it as an
 # enumerate object, which can be used to iterate over both the index and the value at the same time.
+from numpy._core.defchararray import lower
+from numpy._core.strings import upper
 
 # Syntax:
 iterable = ""
@@ -145,3 +147,95 @@ for i, item in enumerate(items):
 # complex iteration tasks.
 # The start parameter gives you control over where to begin the index, providing flexibility when
 # working with enumerations.
+
+
+# ************************* EXERCISE USING enumerate() FUNCTION ************************************
+# #1 You are given a list of student names. Write a program to print each student's name along
+# with their index, starting from 1
+
+stu_names = ["Erick Ondiwa", "James Wasonga", "Felix Awere", "Shadrack Mutinda", "Mark Emmanuel"]
+for idx, stu_name in enumerate(stu_names, start=1):
+    print(f"{idx}. {stu_name}")
+
+
+# #2 You are given a list of numbers. Write a program that increments every number located at an
+# even index by 2.
+
+numbers = [3, 4, 7, 12, 17, 20, 26, 41, 50]
+for even_idx, number in enumerate(numbers):
+    if even_idx % 2 == 0:
+        number += 2
+        print(number)
+    # if numbers[even_idx] % 2 == 0:
+    #
+
+# #3 You are given a list of words. Write a program that prints out all the words that are
+# located at an odd index in the list.
+
+vocabularies = ["patience", "kindness", "tolerance", "trustworthy", "adaptability"]
+for voc_idx, vocabulary in enumerate(vocabularies):
+    if voc_idx % 2 != 0:
+        print(f"{voc_idx}. {vocabulary}")
+
+
+# #4You are given a list of cities. Create a dictionary where the keys are the indices of the
+# cities (starting from 1) and the values are the city names.
+
+kenya_cities = ["Nairobi", "Kisumu", "Mombasa", "Nakuru", "Migori"]
+for city_idx, kenya_city in enumerate(kenya_cities):
+    print(f"{city_idx}. {kenya_city}")
+
+
+# #5 Given a 2D list (matrix), write a program that prints the index of each element in the matrix
+# along with its value.
+
+list_of_ints = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for int_idx, list_of_int in enumerate(list_of_ints):
+    for int_index, ints in enumerate(list_of_int):
+        print(f"Row {int_idx} Column {int_idx}: {ints}")
+
+
+# #6 You are given a list of temperatures recorded over a week. If the temperature is found to be
+# less than 10°C, increase it by 5°C. Use the index to check which temperatures should be adjusted.
+
+weekly_temps = [12, 7, 9, 15, 6, 18, 20]
+
+for temp_idx, weekly_temp in enumerate(weekly_temps):
+    if weekly_temps[temp_idx] < 10:
+        weekly_temp += 5
+    print(f"{temp_idx}. {weekly_temp}")
+
+
+# #7 You are given a string of lowercase letters. Write a program to capitalize every letter
+# located at an odd index.
+
+group_name = "codecrafter"
+for letter_idx, each_letter in enumerate(group_name):
+    if letter_idx % 2 != 0:
+        each_letter = upper(each_letter)
+    # print(each_letter, end="")
+
+erick_description = "eRicKondiWathEcoDeCraFteR.Ke"
+for letter_index, every_letter in enumerate(erick_description):
+    if erick_description[letter_index].isupper():
+        every_letter = lower(every_letter)
+    elif erick_description[letter_index].islower():
+        every_letter = upper(every_letter)
+    else:
+        print(every_letter, end="")
+    print(every_letter, end="")
+
+
+# #8 You have a list of tuples containing movie names and their release years. Write a program to
+# print each movie's name along with its release year and its index in the list, starting from 1.
+
+
+movie_details = [("Inception", 2010), ("The Matrix", 1999), ("Interstellar", 2014)]
+for movie_idx, movie_detail in enumerate(movie_details):
+    print(movie_detail)
+
