371b8f36a1d95d8b0e9133d3f926ee9ac96f40ef
erick-ondiwa <erickochieng3860@gmail.com>
2024-11-17 02:00:44 +0300

    Modified

--- 2_diving_into_python/12_python_GUI/intro_to_GUI.py
+++ 2_diving_into_python/12_python_GUI/intro_to_GUI.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt  # This class is used for alignments
 
 
 class MainWindow(QMainWindow):
@@ -8,6 +9,31 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("GUI Application")
         self.setGeometry(700, 300, 200, 200)
+        self.setWindowIcon(QIcon(" "))  # Add a relevant file path to your Image Icon
+
+        # Adding Labels
+        label1 = QLabel("Erick Ondiwa", self)
+        label1.setGeometry(0, 0, 200, 50)
+        label1.setFont(QFont("Roboto", 30))
+        label1.setStyleSheet("color: blue;"
+                             "background-color: #6fdcf7;"
+                             "text-decoration: underline")  # You can use RGB or HEX values
+
+        # Setting Alignments
+        # label1.setAlignment(Qt.AlignTop)  # Vertically aligns text to the top
+        # label1.setAlignment(Qt.AlignBottom)  # Vertically aligns text to the bottom
+        # label1.setAlignment(Qt.AlignVCenter)  # Vertically aligns text to the center
+        #
+        # label1.setAlignment(Qt.AlignRight)  # horizontally aligns text to the right
+        # label1.setAlignment(Qt.AlignHCenter)  # horizontally aligns text to the center
+        # label1.setAlignment(Qt.AlignLeft)  # horizontally aligns text to the left
+        #
+        # label1.setAlignment(Qt.AlignHCenter | Qt.AlignTop)  # horizontally aligns text to the
+        # # center
+        # label1.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)  # horizontally and vertically
+        # aligns text to the center
+        label1.setAlignment(Qt.AlignCenter)  # horizontally and vertically aligns text to the center
+
 
 
 def main():
