138db88fdda1da92b2d6b91398f8ab9389acd73b
erick-ondiwa <erickochieng3860@gmail.com>
2024-11-21 22:49:19 +0300

    Modified

--- practice_projects/3_digital_clock.py
+++ practice_projects/3_digital_clock.py
@@ -7,6 +7,7 @@ class Clock(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.timer = QTimer(self)
         self.time_label = QLabel("12:00:00", self)
 
         self.initUI()
@@ -16,6 +17,17 @@ class Clock(QWidget):
         hbox.addWidget(self.time_label)
 
         self.setLayout(hbox)
+
+        # Connecting the timer function with the time function
+        self.timer.timeout.connect(self.get_time)
+        self.timer.start(1000)
+
+        self.get_time()
+
+    def get_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss")
+
+        self.time_label.setText(current_time)
 
 
 def main():
