8491eb46b181bd91d23b46eff242bc42f5fc24a6
erick-ondiwa <erickochieng3860@gmail.com>
2024-11-26 22:39:17 +0300

    Modified

--- practice_projects/4_stopwatch.py
+++ practice_projects/4_stopwatch.py
@@ -1,23 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QLabel, QVBoxLayout,
+                             QHBoxLayout, QPushButton)
 from PyQt5.QtCore import QTime, QTimer, Qt
 
 
-class Clock(QWidget):
+class Clock(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.start_button = QPushButton("START", self)
+        self.stop_button = QPushButton("STOP", self)
+        self.reset_button = QPushButton("RESET", self)
 
         self.initUI()
 
     def initUI(self):
-        hbox = QHBoxLayout()
+        # Create a central widget for the main window
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
 
-        self.setLayout(hbox)
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+
+        hbox.addWidget(self.start_button)
+        hbox.addWidget(self.stop_button)
+        hbox.addWidget(self.reset_button)
+
+        vbox.addWidget(hbox)
+
+        central_widget.setLayout(vbox)
 
 
 def main():
     application = QApplication(sys.argv)
-    clock = Clock()
+    window = Clock()
 
-    clock.show()
+    window.show()
     sys.exit(application.exec_())
+
+
+if __name__ == "__main__":
+    main()
